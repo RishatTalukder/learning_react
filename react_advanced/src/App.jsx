f514fca32a348c12f749bf3c5b9cfb41c0dd0a57
Md. Rishat Talukder <talukderrishat2@gmail.com>
2024-02-26 15:20:55 +0600

    tutorial for advanced react

---
+++ react_advanced/src/App.jsx
@@ -0,0 +1,11 @@
+import ErrorExample from './tutorial/01-useState/starter/01-error-example';
+
+function App() {
+  return (
+    <div className='container'>
+      <ErrorExample />
+    </div>
+  );
+}
+
+export default App;
---
+++ react_advanced/src/tutorial/01-useState/starter/01-error-example.jsx
@@ -0,0 +1,6 @@
+const ErrorExample = () => {
+  let count = 0;
+  return <h2>{count}</h2>;
+};
+
+export default ErrorExample;
