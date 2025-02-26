f6caf661280f8a18b65ec5a1840e0956393e5653
Md. Rishat Talukder <120198784+RishatTalukder@users.noreply.github.com>
2025-02-26 10:36:27 +0600

    .

--- PART-1(Fundamentals)/product_list/src/ProductList.js
+++ PART-1(Fundamentals)/product_list/src/ProductList.js
@@ -1,15 +1,28 @@
 import React from "react";
 import Product from "./components/Product";
 
+const product1 = {
+  name: "AMD Ryzen 5 5600X",
+  price: "11,500",
+}; // Product 1
+
+const product2 = {
+  name: "NVIDIA RTX 3060",
+  price: "45,000",
+}; // Product 2
+
+const product3 = {
+  name: "Corsair Vengeance RGB Pro 16GB",
+  price: "6,500",
+}; // Product 3
+
 const ProductList = () => {
   return (
     <div>
       <h1>Product List</h1>
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
+      <Product {...product1} />
+      <Product {...product2} />
+      <Product {...product3} />
     </div>
   );
 };
--- PART-1(Fundamentals)/product_list/src/components/Product.js
+++ PART-1(Fundamentals)/product_list/src/components/Product.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-const Product = () => {
+const Product = (props) => {
+  console.log(props);
   return (
     <div>
       <h1>Image</h1>
-      <h2>Product Name</h2>
-      <p>Price</p>
+      <h2>{props.name}</h2>
+      <p>Price: ${props.price}</p>
     </div>
   );
 };
