33a17099ed9fd7b4db4354381dfaf233fa56f448
Md. Rishat Talukder <120198784+RishatTalukder@users.noreply.github.com>
2025-02-27 16:42:14 +0600

    .

--- PART-1(Fundamentals)/product_list/src/ProductList.js
+++ PART-1(Fundamentals)/product_list/src/ProductList.js
@@ -1,28 +1,32 @@
 import React from "react";
 import Product from "./components/Product";
 
-const product1 = {
-  name: "AMD Ryzen 5 5600X",
-  price: "11,500",
-}; // Product 1
-
-const product2 = {
-  name: "NVIDIA RTX 3060",
-  price: "45,000",
-}; // Product 2
-
-const product3 = {
-  name: "Corsair Vengeance RGB Pro 16GB",
-  price: "6,500",
-}; // Product 3
+const products = [
+  {
+    name: "AMD Ryzen 5 5600X",
+    price: "11,500",
+  },
+  {
+    name: "NVIDIA RTX 3060",
+    price: "45,000",
+  },
+  {
+    name: "Corsair Vengeance RGB Pro 16GB",
+    price: "6,500",
+  },
+];
 
 const ProductList = () => {
+
+  const newProducts = products.map((product, index) => {
+    return (
+      <Product key={index} {...product} />
+    )
+  })
+
   return (
     <div>
-      <h1>Product List</h1>
-      <Product {...product1} />
-      <Product {...product2} />
-      <Product {...product3} />
+      {newProducts}
     </div>
   );
 };
